505e29e10af3f3e26a5a0532b476ea3276a41320
checkeye515 <81191181+checkeye515@users.noreply.github.com>
2021-04-16 14:07:25 -0400

    Update Detect-DCUVersion.ps1

--- Detect-DCUVersion.ps1
+++ Detect-DCUVersion.ps1
@@ -1,2 +1,23 @@
-$App = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -like "Dell Command*"}
-return $App.version
+if(Test-Path -Path "C:\Program Files\Dell\CommandUpdate\dellcommandupdate.exe"){ 
+    $Path = "C:\Program Files\Dell\CommandUpdate"
+    $PathExists = $True }
+
+if(Test-Path -Path "C:\Program Files (x86)\Dell\CommandUpdate"){ 
+    $Path = "C:\Program Files (x86)\Dell\CommandUpdate"
+    $PathExists = $True }
+
+if(Test-Path -Path "C:\Program Files\WindowsApps\Dellinc.DellCommandUpdate*"){ 
+    $DCUVersion = "3.0"
+    return $DCUVersion}
+
+else { $PathExists = $False }
+
+If($Pathexists){
+    $Path = "C:\Program Files (x86)\Dell\CommandUpdate\dellcommandupdate.exe"
+    $DCUVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($Path) | Select -ExpandProperty FileVersion
+    return $DCUVersion
+}
+else {
+   $DCUVersion = "3.0"
+   return $DCUVersion
+}
