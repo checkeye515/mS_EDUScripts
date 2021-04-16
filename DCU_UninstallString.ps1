09c3a71fa145ea0a985451cb14fdee1653a191ed
checkeye515 <81191181+checkeye515@users.noreply.github.com>
2021-04-16 16:02:47 -0400

    Update DCU_UninstallString.ps1

--- DCU_UninstallString.ps1
+++ DCU_UninstallString.ps1
@@ -1,7 +1,7 @@
 $UninstallString = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall |Get-ItemProperty |Where-Object {$_.DisplayName -like "Dell Command | Update*"} |Select-Object -expandproperty UninstallString 
 if($UninstallString.length -eq '0') {
-    $UnisntallString = Get-ChildItem -Path HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall |Get-ItemProperty |Where-Object {$_.DisplayName -like "Dell Command | Update*"} |Select-Object -expandproperty UninstallString 
+    $UninstallString = Get-ChildItem -Path HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall |Get-ItemProperty |Where-Object {$_.DisplayName -like "Dell Command | Update*"} |Select-Object -expandproperty UninstallString 
 }
 
 $UninstallString
-& cmd /c $UnstallString /qn
+& cmd /c $UninstallString /qn
