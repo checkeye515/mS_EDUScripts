1f6f406fa7095a0cef10d6fedc322f68fe27d821
checkeye515 <81191181+checkeye515@users.noreply.github.com>
2021-04-19 13:23:54 -0400

    Update DCU_Update.ps1

--- DCU_Update.ps1
+++ DCU_Update.ps1
@@ -15,7 +15,6 @@ else { $PathExists = $False }
 
 If($Pathexists){
     $DCUVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($Path) | Select -ExpandProperty FileVersion
-    return $DCUVersion
     
     $UninstallString = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall |Get-ItemProperty |Where-Object {$_.DisplayName -like "Dell Command | Update*"} |Select-Object -expandproperty UninstallString 
         if($UninstallString.length -eq '0') {
