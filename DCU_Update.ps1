b3fb9a94e77755a65270fd177cd64c018b43f639
checkeye515 <81191181+checkeye515@users.noreply.github.com>
2021-04-20 12:42:20 -0400

    Update DCU_Update.ps1

--- DCU_Update.ps1
+++ DCU_Update.ps1
@@ -28,7 +28,7 @@ if($DCUVersion -lt 4){
     $argumentList = $UninstallString.Split()[1] + " /qn"
     Start-Process -Wait -FilePath "C:\Windows\System32\msiexec.exe" -ArgumentList $argumentList
 
-    Invoke-WebRequest "https://dl.dell.com/FOLDER06986400M/2/Dell-Command-Update-Application_P5R35_WIN_4.1.0_A00.EXE" -OutFile "C:\kworking\k01\Dell-Command-Update-Application_P5R35_WIN_4.1.0_A00.EXE"
+    #Invoke-WebRequest "https://dl.dell.com/FOLDER06986400M/2/Dell-Command-Update-Application_P5R35_WIN_4.1.0_A00.EXE" -OutFile "C:\kworking\k01\Dell-Command-Update-Application_P5R35_WIN_4.1.0_A00.EXE"
     $InstallDCU = "c:\kworking\k01\Dell-Command-Update-Application_P5R35_WIN_4.1.0_A00.EXE"# l=C:\kworking\k01\DCUInstallLog.txt"
     Start-Process -Wait -FilePath $InstallDCU -ArgumentList "/s" -PassThru
 }
